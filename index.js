45e41b8d58175cefda931c01d827f5e6d7c54812
goblin.brn <bschenck0@gmail.com>
2022-05-13 19:51:47 -0700

    setup basic structure and imports

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+// import dependencies
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+// import modules
+const outputHtml = require('./src/outputHtml')
+let output = path.resolve(__dirname, "dist", "employees.html")
+
+// import classes
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+const Manager = require('./Manager');
+
+const team = [];
 
 function generateProfiles() {
     inquirer.prompt([
         {
-
+            
         }
     ])
 }
 
 generateProfiles();
+
+function createTeam() {
+    fs.writeFileSync(output, outputHtml(team), 'utf-8');
+    console.log("HTML Generated. Check dist.");
+}
